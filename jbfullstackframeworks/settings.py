bfefb3405e68f49f146b9cc9f5c2e3dea80503a7
JackBen2019 <jackkbennett17@gmail.com>
2020-04-28 08:44:20 +0000

    Database link added into settings.py

--- jbfullstackframeworks/settings.py
+++ jbfullstackframeworks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,13 +74,14 @@ WSGI_APPLICATION = 'jbfullstackframeworks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
 
+DATABASES = {'default': dj_database_url.parse("postgres://saynigjqnymila:fb7b529a289586bfeee0cb0e2b5a0c0fcadb9fc41af026e04f081ec0aa5466f2@ec2-54-217-213-79.eu-west-1.compute.amazonaws.com:5432/d8i9frvqusbrkt")}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
