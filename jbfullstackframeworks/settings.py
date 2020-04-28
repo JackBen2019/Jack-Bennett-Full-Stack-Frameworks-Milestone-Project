33f01d9e396002e1216c80e370a360c5c0735dff
JackBen2019 <jackkbennett17@gmail.com>
2020-04-28 17:58:40 +0000

    Updated env.py file to contain secret key

--- jbfullstackframeworks/settings.py
+++ jbfullstackframeworks/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from os import path
 import dj_database_url
+
+if os.path.exists("env.py"):
+   import env
 
 if os.environ.get('DEVELOPMENT'):
     development = True
@@ -26,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'sc%drq-!4elvzql_ov-xye1$iza)(8r5e@wq4rhqj=183gyvqg')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 development = os.environ.get("DEVELOPMENT")
@@ -84,15 +88,18 @@ WSGI_APPLICATION = 'jbfullstackframeworks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-if development:
-    DATABASES = {
-    'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
- }
-else:
+if "DATABASE_URL" in os.environ:
     DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+else:
+    print("Database URL not found. Using SQLite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
