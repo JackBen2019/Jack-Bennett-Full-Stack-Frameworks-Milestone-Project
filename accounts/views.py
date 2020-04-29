727ec43e437ede4630d39e969c6ebd8d9211f85d
JackBen2019 <jackkbennett17@gmail.com>
2020-04-29 08:23:45 +0000

    Extra funtionality added to login form such as successful and failure login messages

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from accounts.forms import UserLoginForm
 
-
 def index(request):
     """Return the index.html file"""
     return render(request,  'index.html')
@@ -14,7 +13,22 @@ def logout(request):
     messages.success(request, "You have successfully been logged out")
     return redirect(reverse('index'))
 
+
 def login(request):
-    """Request to log in"""
-    login_form = UserLoginForm()
-    return render(request, 'login.html', {"login_form": login_form})
+    """Return a login page"""
+    if request.method == "POST":
+        login_form = UserLoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                    password=request.POST['password'])
+            
+
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!")
+            else:
+                login_form.add_error(None, "Your username or password is incorrect")
+    else:
+        login_form = UserLoginForm()
+    return render(request, 'login.html', {'login_form': login_form})
