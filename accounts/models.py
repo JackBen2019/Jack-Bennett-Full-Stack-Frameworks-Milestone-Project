cbeefc8bda90e3480b24eb6b9d096f14a164b24a
JackBen2019 <jackkbennett17@gmail.com>
2020-05-25 09:00:22 +0000

    Total orders and orders pending boxes added to profile page

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
 
 class Post(models.Model):
     """
--- accounts/views.py
+++ accounts/views.py
@@ -74,6 +74,7 @@ def registration(request):
 
 def user_profile(request):
     """The user's profile page"""
+    
     user = User.objects.get(email=request.user.email)
     return render(request, 'profile.html', {"profile": user})
 
