c69ed8cd77af50c2663c596dd76acb10f122fd6c
JackBen2019 <jackkbennett17@gmail.com>
2020-06-07 15:20:16 +0000

    Email port updated to 25 in settings.py file

--- jbfullstackframeworks/settings.py
+++ jbfullstackframeworks/settings.py
@@ -187,7 +187,7 @@ MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_HOST = "localhost"
-EMAIL_PORT = 1025
+EMAIL_PORT = 25
 EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 
