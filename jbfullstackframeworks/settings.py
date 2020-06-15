9ef5c96d6ef99a45bd9462f71bee90653fcd01e2
JackBen2019 <jackkbennett17@gmail.com>
2020-06-15 08:53:58 +0000

    EMAIL_BACKEND value updated

--- jbfullstackframeworks/settings.py
+++ jbfullstackframeworks/settings.py
@@ -189,7 +189,7 @@ STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
 # Email Configuration
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
