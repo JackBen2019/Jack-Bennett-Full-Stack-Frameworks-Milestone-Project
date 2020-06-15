a1b70d11cc6bb978106a4bf56656894ecfbe8a65
JackBen2019 <jackkbennett17@gmail.com>
2020-06-15 12:46:47 +0000

    Password reset links updated for use on REAL emails

--- accounts/url_reset.py
+++ accounts/url_reset.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.views import (
     password_reset,
     password_reset_done,
@@ -6,11 +7,15 @@ from django.contrib.auth.views import (
     password_reset_complete)
 
 urlpatterns = [
-    url(r'^$', password_reset, name='password_reset'),
+    url(r'^$', password_reset,
+        {'post_reset_redirect': reverse_lazy('password_reset_done')},
+        name='password_reset'),
     url(r'^reset-password/done/$', password_reset_done,
         name='password_reset_done'),
     url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
-        password_reset_confirm, name='password_reset_confirm'),
+        password_reset_confirm,
+        {'post_reset_redirect': reverse_lazy('password_reset_complete')},
+        name='password_reset_confirm'),
     url(r'^complete/$', password_reset_complete,
         name='password_reset_complete'),
 ]
