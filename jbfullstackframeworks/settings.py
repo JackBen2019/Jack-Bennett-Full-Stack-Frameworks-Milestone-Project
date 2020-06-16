b6481b75cfd238c0177d3f24e4f207e4bc8e46f2
JackBen2019 <jackkbennett17@gmail.com>
2020-06-16 06:01:44 +0000

    EMAIL_BACKEND option removed

--- jbfullstackframeworks/settings.py
+++ jbfullstackframeworks/settings.py
@@ -189,7 +189,6 @@ STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
 # Email Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
